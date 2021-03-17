40a6ab957651dc6930e4aed49dd032ce8be11087
Drilon <50522333+drilonsaiti@users.noreply.github.com>
2021-03-17 11:31:50 +0100

    Bug fixes.

--- src/main/java/airlanetickets/repository/PaymentRepository.java
+++ src/main/java/airlanetickets/repository/PaymentRepository.java
@@ -3,5 +3,6 @@ package airlanetickets.repository;
 import airlanetickets.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment,Long> {
 }
